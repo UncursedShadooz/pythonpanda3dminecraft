5ba7b29cc0856c83fe1c273a5fec928c65995f65
4dri4n4awesomesauce <adrilivia.m@gmail.com>
2026-05-31 09:50:14 +0700

    visuals

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 from direct.showbase.ShowBase import ShowBase
+from mapmanager import Mapmanager
 
 class Minecraft(ShowBase):
     def __init__(self):
         ShowBase.__init__(self)
+        self.land = Mapmanager()
+        base.camLens.setFov(90)
         
 game = Minecraft()
 game.run()
